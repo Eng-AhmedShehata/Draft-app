3914e7d23b3e38de15e163099d754f2221a751a6
Ahmed Shehata <ahamdyshehata@gmail.com>
2020-02-17 15:30:52 +0200

    rename main activity

--- app/src/main/java/com/ashehata/draftapp/UsersActivity.kt
+++ app/src/main/java/com/ashehata/draftapp/UsersActivity.kt
@@ -3,10 +3,10 @@ package com.ashehata.draftapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class UsersActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_users)
     }
 }
